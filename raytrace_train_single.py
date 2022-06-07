8ba7747b14950ad05addbfabdd8c169d7b297723
anton <anton.holmberg@live.com>
2022-06-07 17:26:49 +0200

    Added documentation and comments

--- raytrace_train_single.py
+++ raytrace_train_single.py
@@ -1,12 +1,11 @@
+""" For training a single model for the raytracing. Tested wit htensorflow 2.2
+"""
 import os
 from gpuutils import GpuUtils
-from sklearn.preprocessing import MinMaxScaler
 import raytrace_model_def
-from pickle import dump
 import tensorflow as tf
 import pandas as pd
 from tensorflow import keras
-from tensorflow.keras import layers
 import numpy as np
 
 GpuUtils.allocate(gpu_count=1, framework='keras')
@@ -15,10 +14,10 @@ physical_devices = tf.config.list_physical_devices('GPU')
 for device in physical_devices:
     tf.config.experimental.set_memory_growth(device, True)
 
-np.set_printoptions(precision=3, suppress=True)
 
+#tf.keras.backend.set_floatx('float64')
 
-"""  # Cartesian/polar coordinates
+"""  # Cartesian/cylindrical coordinates
 df = pd.read_csv('/mnt/md0/aholmberg/data/raytrace_samples_sobol_24.csv')
 
 sc_pos_r = df['source_pos_r'].to_numpy().astype(np.float64)
@@ -43,7 +42,7 @@ unique, index, count = np.unique(x_new, return_counts=True,
  """
 
 # polar/spherical coordinates -------------------------------------------------
-df = pd.read_pickle('/mnt/md0/aholmberg/data/raytracing_24_spherical.pkl')
+df = pd.read_pickle('/mnt/md0/aholmberg/data/raytracing_random_25_spherical.pkl')
 
 sc_pos_d = df['source_pos_d'].to_numpy().astype(np.float64)
 sc_pos_phi = df['source_pos_phi'].to_numpy().astype(np.float64)
@@ -77,19 +76,15 @@ for i in range(4):
 
 y_train = y_new
 
-# scaler_x = MinMaxScaler(feature_range=(0, 1))
-# scaler_x.fit(x_train)
-# norm_x_train = scaler_x.transform(x_train)
-print(x_train.shape)
-print(y_train.shape)
+# Delete traning points with phi larger than 179 degrees
+index = x_train[:, 1] > 179
+x_train = np.delete(x_train, index, axis=0)
+y_train = np.delete(y_train, index, axis=0)
+
 norm_x_train = np.zeros_like(x_train)
 norm_x_train[:, 0] = x_train[:, 0] / (np.sqrt(2700**2 + 2000**2))
 norm_x_train[:, 1] = x_train[:, 1] / (180)
 norm_x_train[:, 2] = x_train[:, 2] / -(200)
-
-# scaler_y = MinMaxScaler(feature_range=(0, 1))
-# scaler_y.fit(y_train)
-# norm_y_train = scaler_y.transform(y_train)
 
 norm_y_train = np.zeros_like(y_train)
 norm_y_train[:, 0] = y_train[:, 0] / (20000)
@@ -101,51 +96,47 @@ norm_y_train[:, 5] = (y_train[:, 5] - 90) / (90)
 norm_y_train[:, 6] = y_train[:, 6] / (180)
 norm_y_train[:, 7] = y_train[:, 7] / (90)
 
-depth = 4
-width = 60
+depth = 14
+width = 50
 
-model_name = f'ResNetFc_w{width}_d{depth}_vwidth1_news_selu_ln'
+# Set the name of the model and directory to save the model in
+model_name = f'ResNetFc_w{width}_d{depth}_vwidth20_vdepth4_news_lrelu_ln_179deg_250_np'
 path = '/mnt/md0/aholmberg/models/' + model_name
 
-scaler_path_x = path + "-x-scaler.pkl"
-scaler_path_y = path + "-y-scaler.pkl"
+opt = keras.optimizers.Adam(learning_rate=5e-4)
+act = keras.layers.LeakyReLU()
+kernel_init = 'he_normal'
 
-#dump(scaler_x, open(scaler_path_x, 'wb'))
-#dump(scaler_y, open(scaler_path_y, 'wb'))
-
-opt = keras.optimizers.Adam(learning_rate=1e-3)
-# act = layers.LeakyReLU(alpha=0.3)
-act = 'selu'
-kernel_init = keras.initializers.lecun_normal()
-ln = True
-model = raytrace_model_def.get_resnet_model_new(
+# Define the model
+model = raytrace_model_def.get_resnet_model_lrelu(
     width=width,
     depth=depth,
     activation=act,
     kernel_init=kernel_init,
-    ln=ln,
     optimizer=opt,
     )
 
 print(model.summary())
 
-
+# lr scheduler
 def scheduler(epoch, lr):
     if epoch <= 3:
-        return 1e-3
+        return 5e-4
     else:
-        return lr * tf.math.exp(-0.05)
+        return lr * tf.math.exp(-0.03)
 
 
 lr_scheduler = keras.callbacks.LearningRateScheduler(scheduler)
 
+# craete the directory for checkpointing models
 checkpoint_filepath = '/mnt/md0/aholmberg/models/' + model_name
 if not os.path.isdir(checkpoint_filepath):
     os.mkdir(checkpoint_filepath)
 
-logdir = './logs/' + model_name
-# profile_batch='1000,1020') does not work over ssh with tf version <2.5
-tb_callback = tf.keras.callbacks.TensorBoard(logdir, update_freq=1)
+# set logs for tensorboard. Performance counters does not work on remote machine with tensorflow<2.5
+# logdir = './logs/' + model_name
+# profile_batch does not work over ssh with tf version <2.5
+# tb_callback = tf.keras.callbacks.TensorBoard(logdir, update_freq=1)
 
 model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
     filepath=checkpoint_filepath,
@@ -157,20 +148,21 @@ history = model.fit(
     norm_x_train,
     [norm_y_train[:, :2], norm_y_train[:, 2:4],
      norm_y_train[:, 4:6], norm_y_train[:, 6:]],
-    epochs=180,
+    epochs=300,
     batch_size=512,
     validation_split=0.1,
-    callbacks=[model_checkpoint_callback, lr_scheduler, tb_callback],
+    callbacks=[model_checkpoint_callback, lr_scheduler],  # , tb_callback],
     shuffle=True
     )
 
+# Load and save the best epoch
 model.load_weights(checkpoint_filepath)
 
 if not os.path.isdir(path):
     os.mkdir(path)
 
 model.save(path)
-
+# Save loss history
 loss_df = pd.DataFrame(history.history)
 path_to_loss = 'losses/history_' + model_name + '.pkl'
 loss_df.to_pickle(path_to_loss)
